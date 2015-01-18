8088755489823142690c6cab51a1d2a5d6a17d69
Philipp Frenzel <pf@com-x-cha.com>
2015-01-18 21:06:35 +0100

    add external drag n drop sample
    added sample for dragin external events

--- views/site/index.php
+++ views/site/index.php
@@ -6,6 +6,29 @@ use yii\web\JsExpression;
 
 /* @var $this yii\web\View */
 $this->title = 'yii2 extension yii2-fullcalendar demo';
+
+$DragJS = <<<EOF
+/* initialize the external events
+-----------------------------------------------------------------*/
+
+$('#external-events .fc-event').each(function() {
+    // store data so the calendar knows to render an event upon drop
+    $(this).data('event', {
+        title: $.trim($(this).text()), // use the element's text as the event title
+        stick: true // maintain when user navigates (see docs on the renderEvent method)
+    });
+    // make the event draggable using jQuery UI
+    $(this).draggable({
+        zIndex: 999,
+        revert: true,      // will cause the event to go back to its
+        revertDuration: 0  //  original position after the drag
+    });
+});
+
+EOF;
+
+$this->registerJs($DragJS);
+
 ?>
 <div class="site-index">
 
@@ -27,25 +50,35 @@ $this->title = 'yii2 extension yii2-fullcalendar demo';
 
     <div class="body-content">
 
-    <?php
+<?php
 
-    $JSCode = <<<EOF
+$JSCode = <<<EOF
 function(start, end) {
-        var title = prompt('Event Title:');
-        var eventData;
-        if (title) {
-            eventData = {
-                title: title,
-                start: start,
-                end: end
-            };
-            $('#w0').fullCalendar('renderEvent', eventData, true);
-        }
-        $('#w0').fullCalendar('unselect');
+    var title = prompt('Event Title:');
+    var eventData;
+    if (title) {
+        eventData = {
+            title: title,
+            start: start,
+            end: end
+        };
+        $('#w0').fullCalendar('renderEvent', eventData, true);
+    }
+    $('#w0').fullCalendar('unselect');
 }
 EOF;
 
-    $JSEventClick = <<<EOF
+$JSDropEvent = <<<EOF
+function(date) {
+    alert("Dropped on " + date.format());
+    if ($('#drop-remove').is(':checked')) {
+        // if so, remove the element from the "Draggable Events" list
+        $(this).remove();
+    }
+}
+EOF;
+
+$JSEventClick = <<<EOF
 function(calEvent, jsEvent, view) {
 
     alert('Event: ' + calEvent.title);
@@ -56,14 +89,31 @@ function(calEvent, jsEvent, view) {
     $(this).css('border-color', 'red');
 
 }
+
 EOF;
     
     ?>
+
+<div id="external-events">
+    <h4>Draggable Events</h4>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 1</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 2</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 3</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 4</div>
+    <div class="fc-event ui-draggable ui-draggable-handle">My Event 5</div>
+    <p>
+        <input type="checkbox" id="drop-remove">
+        <label for="drop-remove">remove after drop</label>
+    </p>
+</div>
 
         <?= yii2fullcalendar\yii2fullcalendar::widget(array(
               'clientOptions' => [
                     'selectable' => true,
                     'selectHelper' => true,
+                    'droppable' => true,
+                    'editable' => true,
+                    'drop' => new JsExpression($JSDropEvent),
                     'select' => new JsExpression($JSCode),
                     'eventClick' => new JsExpression($JSEventClick),
                     'defaultDate' => date('Y-m-d')
